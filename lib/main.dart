d7aedd9d03bbcede750e0ceb54363805ceaa5a50
Rob <dreamYiZ@outlook.com>
2021-11-16 00:06:44 +0800

    card

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+
+
+Card buildCard() {
+  var heading = '\$2300 per month';
+  var subheading = '2 bed, 1 bath, 1300 sqft';
+  var cardImage = NetworkImage(
+      'https://source.unsplash.com/random/800x600?house');
+  var supportingText =
+      'Beautiful home to rent, recently refurbished with modern appliances...';
+  return Card(
+      elevation: 4.0,
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(heading),
+            subtitle: Text(subheading),
+            trailing: Icon(Icons.favorite_outline),
+          ),
+          Container(
+            height: 200.0,
+            child: Ink.image(
+              image: cardImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.centerLeft,
+            child: Text(supportingText),
+          ),
+          ButtonBar(
+            children: [
+              TextButton(
+                child: const Text('CONTACT AGENT'),
+                onPressed: () {/* ... */},
+              ),
+              TextButton(
+                child: const Text('LEARN MORE'),
+                onPressed: () {/* ... */},
+              )
+            ],
+          )
+        ],
+      ));
+}
+
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -88,7 +135,8 @@ class _RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
-          return _buildRow(_suggestions[index]);
+          return buildCard();
+          // return _buildRow(_suggestions[index]);
         });
   }
 
